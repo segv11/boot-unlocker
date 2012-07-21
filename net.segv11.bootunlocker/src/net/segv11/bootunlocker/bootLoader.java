620541e26489c8fe565b7936b8f8484e71fd9861
James Mason <jmason888@gmail.com>
2012-07-21 15:56:25 -0700

    Pipe commands to "su" rather than giving them as arguments.  This works around some authentication differences between SuperSU and Superuser, and is also considered best practice in dealing with certain su bugs.

--- net.segv11.bootunlocker/src/net/segv11/bootunlocker/bootLoader.java
+++ net.segv11.bootunlocker/src/net/segv11/bootunlocker/bootLoader.java
@@ -3,6 +3,8 @@
  */
 package net.segv11.bootunlocker;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 import android.util.Log;
@@ -30,9 +32,9 @@ public class bootLoader {
 	/** Private constants for working with the lock state in the param partition
 	 */
 	private static final String queryCommand =
-		"dd ibs=1 count=1 skip=124 if=/dev/block/platform/omap/omap_hsmmc.0/by-name/param"; 
+		"dd ibs=1 count=1 skip=124 if=/dev/block/platform/omap/omap_hsmmc.0/by-name/param  # query "; 
 	private static final String writeCommand =
-	     "dd obs=1 count=1 seek=124 of=/dev/block/platform/omap/omap_hsmmc.0/by-name/param";
+	     "dd obs=1 count=1 seek=124 of=/dev/block/platform/omap/omap_hsmmc.0/by-name/param # write ";
 	
     /** checks if we know how to lock/unlock the bootloader on this device */
     public static boolean checkCompatibleDevice() {
@@ -63,8 +65,16 @@ public class bootLoader {
     		Log.i(TAG, "Unlocking bootloader by sending " + outByte + " to " + writeCommand);
     	}
     	
-		Process p = Runtime.getRuntime().exec(new String[]{"su", "-c", writeCommand});
-		p.getOutputStream().write(outByte);
+		Process p = Runtime.getRuntime().exec("su");
+	    DataOutputStream w=new DataOutputStream(p.getOutputStream());
+	    // BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
+	    w.writeBytes(writeCommand+ outByte +"\n");
+	    w.flush();
+	    w.writeByte(outByte);
+	    w.flush();
+	    w.writeBytes("exit\n");
+	    w.flush();
+	    w.close();				
     }
  
     
@@ -76,9 +86,23 @@ public class bootLoader {
 		
 		try {
     		Log.v(TAG, "Getting bootloader state with " + queryCommand);
-			Process p = Runtime.getRuntime().exec(new String[]{"su", "-c", queryCommand});
-			int isLocked = p.getInputStream().read();
-    		Log.v(TAG, "Got lock value " + isLocked);
+			//Process p = Runtime.getRuntime().exec(new String[]{"su", "-c", queryCommand});
+			//int isLocked = p.getInputStream().read();
+
+			
+			Process p = Runtime.getRuntime().exec("su");
+		    DataOutputStream w =new DataOutputStream(p.getOutputStream());
+		    DataInputStream r = new DataInputStream(p.getInputStream());
+		    
+		    w.writeBytes(queryCommand+"\n");
+		    w.flush();
+		    int isLocked = r.readByte();
+		    w.writeBytes("exit\n");
+		    w.flush();
+		    w.close();				
+			
+			
+			Log.v(TAG, "Got lock value " + isLocked);
 			if (isLocked == 1) {
 				return BL_LOCKED;
 			} else if (isLocked == 0)  {
