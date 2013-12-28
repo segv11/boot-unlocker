ce5cf92922c3a949f542cadd74d63bda57bc6c5a
James Mason <jmason888@gmail.com>
2013-12-27 22:57:04 -0800

    subtree management commands

---
+++ subtree_push.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# push updates to the documentation wiki
+# do this AFTER a commit and push to the main repo
+#
+# https://github.com/apenwarr/git-subtree/blob/master/git-subtree.txt
+#
+git subtree push --prefix net.segv11.bootunlocker/assets/wiki/ https://code.google.com/p/boot-unlocker-gnex.wiki/ master 
