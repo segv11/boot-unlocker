a7086943558da404da2d81033d0cf53948f4215d
James Mason <jmason888@gmail.com>
2015-01-09 21:15:02 -0800

    Wiki cleanups

--- subtree_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# push updates to the documentation wiki
-# do this AFTER a commit and push to the main repo
-#
-# https://github.com/apenwarr/git-subtree/blob/master/git-subtree.txt
-#
-git subtree push --prefix app/assets/wiki/ https://code.google.com/p/boot-unlocker-gnex.wiki/ master 
